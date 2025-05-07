f79df406135b3cc547e4587cdcc79c27008426e1
Eugene Ndizihiwe <eugenekjyn@gmail.com>
2025-05-07 18:10:38 +0200

    fetcth API exercise

--- xhr.js
+++ xhr.js
@@ -1,23 +1,23 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const SendHttpRequest= (metthod, url) => {
-    const req = new XMLHttpRequest();
-    req.open(method, url);
-
-    req.responseType = 'json'; 
-
-    req.onload = () => {
-        const data = req.response;
-        console.log('data');
-    }
-
-    req.send();
-}
 
 const getData= () => {
-    SendHttpRequest('GET', 'https://reqres.in/api/users');
-}
+
+    fetch('https://fakestoreapi.com/products')
+     .then(response => response.json())
+     
+    
+        /*
+        You cannot 'GET' any URL from the search/address bar using XMLHttpRequest due to browser security restrictions. Here’s why:
+        1. Protocol (e.g., http vs. https)
+        2. Domain (e.g., www.example.com vs. api.example.com)
+        3. Port (e.g., :80 vs. :8080)
+        */
+    }
+    
+    
+
 
 const PostData= () => {}
 
